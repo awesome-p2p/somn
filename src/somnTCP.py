a93849cd729d7c3acd91d8448dd5ae05da61099c
Camille <camilleh@cecs.pdxedu>
2013-11-15 09:15:19 -0800

    Completed Rx and Tx base threads, with loopback test function

--- src/somnTCP.py
+++ src/somnTCP.py
@@ -2,30 +2,53 @@
 
 import socket
 import threading
-import somnConst
-import queue
+#import somnConst
+import Queue
+import sys
 
 class RxThread(threading.Thread):
   def __init__(self, ip, port, RxQ, RxAlive):
     threading.Thread.__init__(self)
     self.ip = ip
     self.port = port
-    self.rxQ = RxQ
+    self.RxQ = RxQ
     self.RxAlive = RxAlive
   def run(self):
-    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    skt.bind((self.ip, self.port))
+    try:
+      skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as msg:
+      print "Rx Thread Failed"
+      skt = None
+      sys.exit(1)
+    
+    try:
+      skt.bind((self.ip, self.port))
+    except socket.error as msg:
+      print "Rx bind failed"
 
-    while threading.isSet(self.RxAlive):
-      skt.listen(1)
+    while (self.RxAlive.is_set() and  (skt != None)):      
+      try:
+        skt.listen(1)
+      except socket.error as msg:
+        skt.close()
+        skt = None
+        self.RxAlive.clear()
+        print "Rx Listen failed" 
+        break 
 
-      con, sourceNodeIp = skt.accept()
+      try:
+        con, sourceNodeIp = skt.accept()
+      except socket.timeout:
+        pass
+      else:
+        while 1:
+          pktRx = con.recv(1024)#somnConst.MAX_PACKET_SIZE)
+          if not pktRx: break
+          self.RxQ.put(pktRx)
+        con.close()
 
-      while 1:
-        pktRx = con.recv(somnConst.MAX_PACKET_SIZE)
-        if not pktRx: break
-        self.RxQ.put(pktRx)
-      con.close()
+    if skt is not None:  skt.close()  
+    print "Rx thread shutting down"
 
 
 class TxThread(threading.Thread):
@@ -34,43 +57,53 @@ class TxThread(threading.Thread):
     self.TxQ = TxQ
     self.TxAlive = TxAlive
   def run(self):
-    while threading.isSet(self.TxAlive):
-      pkt = TxQ.get()
-      IP = pkt.destIP
-      PORT = pkt.destPort
-      skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      skt.connet((IP, PORT))
-      skt.send(pkt.raw())
-      skt.close()
+    while self.TxAlive.is_set():
+      try:
+        pkt = self.TxQ.get(False)
+        self.TxQ.task_done()
+      except Queue.Empty:
+        pass
+      else:
+        #IP = pkt.destIP
+        #PORT = pkt.destPort
+        IP = "127.0.0.1"
+        PORT = 45000
+        skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        skt.connect((IP, PORT))
+        skt.send(pkt)
+        skt.close()
+    
+    print "Tx Thread shutting down"
 
      
 def startSomnRx(RxAlive, RxQ):
   #localhostIp = netifaces.ifaddresses('eth0')[netifaces.AF_INET[0]['addr']
   #localhostPort = somnConst.DEFAULT_PORT
   localhostIp = "127.0.0.1" 
-  localhostPort = 56000
-  somnRx = RxThread(localhostIp, localhostPort, RxQ, RxAlive))
+  localhostPort = 45000
+  somnRx = RxThread(localhostIp, localhostPort, RxQ, RxAlive)
   somnRx.start()
   return somnRx
      
   
 def startSomnTx(TxAlive, TxQ):
-  somnTx = TxThread(TxAlive, TxQ)
+  somnTx = TxThread(TxQ, TxAlive)
   somnTx.start()
   return somnTx
 
-def main():
-  TxQ = queue.queue
-  networkAlive = threading.Event
-  TxAlive.set()
-  RxAlive.set()
+def lbTest():
+  socket.setdefaulttimeout(15)
+  TxQ = Queue.Queue()
+  RxQ = Queue.Queue()
+  networkAlive = threading.Event()
+  networkAlive.set()
   Rx = startSomnRx(networkAlive, RxQ)
   Tx = startSomnTx(networkAlive, TxQ) 
+  testPkt = "Hello SOMN"
   TxQ.put(testPkt)
-  RxQ.get(echoPkt)     
+  echoPkt = RxQ.get()    
+  print echoPkt
+  RxQ.task_done() 
   networkAlive.clear()
   Tx.join()
   Rx.join()
-
-if __name__ == "__main__":
-  main()
