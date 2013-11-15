22ccb94b6b3a3e920aa438e292956a2178ffc67f
Camille <camilleh@cecs.pdxedu>
2013-11-15 09:42:52 -0800

    minor changes to exception catching and behaviour

--- src/somnTCP.py
+++ src/somnTCP.py
@@ -2,9 +2,13 @@
 
 import socket
 import threading
-#import somnConst
+from somnConst import *
 import Queue
 import sys
+
+#LOOPBACK_MODE = 0
+#SOMN_LOOPBACK_IP = "127.0.0.1"
+#SOMN_LOOPBACK_PORT = 45000
 
 class RxThread(threading.Thread):
   def __init__(self, ip, port, RxQ, RxAlive):
@@ -14,20 +18,22 @@ class RxThread(threading.Thread):
     self.RxQ = RxQ
     self.RxAlive = RxAlive
   def run(self):
-    try:
+    try:  # create a socket
       skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as msg:
       print "Rx Thread Failed"
       skt = None
-      sys.exit(1)
-    
-    try:
+      self.RxAlive.clear()
+      exit()
+
+    try:  # bind to localhost ip/port
       skt.bind((self.ip, self.port))
     except socket.error as msg:
       print "Rx bind failed"
+      self.RxAlive.clear()
 
     while (self.RxAlive.is_set() and  (skt != None)):      
-      try:
+      try:  # listen for incoming packets
         skt.listen(1)
       except socket.error as msg:
         skt.close()
@@ -36,11 +42,11 @@ class RxThread(threading.Thread):
         print "Rx Listen failed" 
         break 
 
-      try:
+      try:  # accept connections
         con, sourceNodeIp = skt.accept()
       except socket.timeout:
         pass
-      else:
+      else: # read all data from socket, push onto the Queue
         while 1:
           pktRx = con.recv(1024)#somnConst.MAX_PACKET_SIZE)
           if not pktRx: break
@@ -57,42 +63,57 @@ class TxThread(threading.Thread):
     self.TxQ = TxQ
     self.TxAlive = TxAlive
   def run(self):
-    while self.TxAlive.is_set():
-      try:
+    while self.TxAlive.is_set():  
+      try:  # check for available outgoing packets
         pkt = self.TxQ.get(False)
-        self.TxQ.task_done()
       except Queue.Empty:
         pass
-      else:
-        #IP = pkt.destIP
-        #PORT = pkt.destPort
-        IP = "127.0.0.1"
-        PORT = 45000
-        skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        skt.connect((IP, PORT))
+      else: # send packet to desired peer
+        if LOOPBACK_MODE:
+          IP = SOMN_LOOPBACK_IP
+          PORT = SOMN_LOOPBACK_PORT
+        else:
+          IP = pkt.getIP()
+          PORT = pkt.getPort()
+        try:  # attempt to create a socket, break on failure
+          skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as msg:
+          self.TxAlive.clear()
+          break
+        try:  # attempt to connect to desired node, if fails, generate a bad route event
+          skt.connect((IP, PORT))
+        except socket.error:
+          # this should generate a bad route event
+          break
         skt.send(pkt)
+        self.TxQ.task_done()
         skt.close()
     
     print "Tx Thread shutting down"
 
-     
+
+# Rx thread helper start function     
 def startSomnRx(RxAlive, RxQ):
-  #localhostIp = netifaces.ifaddresses('eth0')[netifaces.AF_INET[0]['addr']
-  #localhostPort = somnConst.DEFAULT_PORT
-  localhostIp = "127.0.0.1" 
-  localhostPort = 45000
+  if LOOPBACK_MODE:
+    localhostIp = SOMN_LOOPBACK_IP
+    localhostPort = SOMN_LOOPBACK_PORT
+  else:
+    localhostIp = getLocalHostIp() #netifaces.ifaddresses('eth0')[netifaces.AF_INET[0]['addr']]
+    localhostPort = somnConst.DEFAULT_PORT
   somnRx = RxThread(localhostIp, localhostPort, RxQ, RxAlive)
   somnRx.start()
   return somnRx
      
-  
+
+# Tx thread helper start function
 def startSomnTx(TxAlive, TxQ):
   somnTx = TxThread(TxQ, TxAlive)
   somnTx.start()
   return somnTx
 
+# simple loopback test function that delivers a hello somn message over the loopback address
 def lbTest():
-  socket.setdefaulttimeout(15)
+  socket.setdefaulttimeout(5)
   TxQ = Queue.Queue()
   RxQ = Queue.Queue()
   networkAlive = threading.Event()
