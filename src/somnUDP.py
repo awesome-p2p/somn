56839baa7a80b947beea7223cd7f6af270bc5cf1
Camille Huffman Day <zombie.grl@gmail.com>
2013-11-16 19:32:07 -0800

    started udp thread listener for enrollment

---
+++ src/somnUDP.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.3
+import threading
+import socket
+import queue
+import time
+import somnPkt
+
+class somnUDPThread(threading.Thread):
+  def __init__(self, enrollPkt, RxQ, networkAlive):
+    threading.Thread.__init__(self)
+    self.enrollPkt = enrollPkt
+    self.RxQ = RxQ
+    self.networkAlive = networkAlive
+  def run(self):
+    udpSkt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udpSkt.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+    udpSkt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+    udpSkt.sendto("Hello There".encode("utf-8"), ('<broadcast>', 45000))
+    udpSkt.close()
+
+    udpSkt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udpSkt.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+    udpSkt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+    udpSkt.bind(('', 45000))
+    while self.networkAlive.is_set():
+      try:
+        data, addr = udpSkt.recvfrom(24)  
+      except socket.error:
+        break
+      packet = somnPkt.somnPacket()
+      packet.decode(data)
+      self.RxQ.put(packet)
+    udpSkt.close()
+if __name__=="__main__":
+  socket.setdefaulttimeout(5)
+  rxdq = queue.Queue()
+  networkAlive = threading.Event()
+  udpThread = somnUDPThread("Hello There", rxdq, networkAlive)
+  networkAlive.set()
+  udpThread.start()
+  time.sleep(30)
+  packet = rxdq.get(timeout = 15)
+  print(packet)
+  networkAlive.clear()
+  udpThread.join()
