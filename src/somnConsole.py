5cbd4bcea64672c11de4d13e110222b3e75bb8f4
pjl <pjl@pdx.edu>
2013-12-05 23:54:56 -0800

    Added a bunch of stuff, can now send msgs from console

--- src/somnConsole.py
+++ src/somnConsole.py
@@ -3,6 +3,8 @@
 import somnMesh
 import curses
 import multiprocessing
+import queue
+import time
 
 uiRunning = True
 
@@ -15,39 +17,133 @@ globalNodeOutput = ipcMgr.list()
 #Node list
 nodeList = ipcMgr.list()
 
+#Node dictionary
+nodeDict = dict()
 
-def startupNewNode():
-  node = somnMesh.CreateNode(nodePrintCallback)
+#pop-up window
+popupwin = None
+
+
+def startupNewNode(txq, rxq, cmdpipe):
+  nodetxq = queue.Queue()
+  noderxq = queue.Queue()
+
+  node = somnMesh.somnMesh(nodetxq, noderxq, nodePrintCallback)
   node.start()
-  nodeList.append(node.nodeID)
+
+  time.sleep(1)
+  #while node._mainLoopRunning:
+  while True:
+    #put anything on txq on nodetxq
+    try:
+      nexttx = txq.get(False)
+      nodetxq.put(nexttx)
+    except:
+      pass
+
+    #put anything on noderxq on rxq
+    try:
+      nextrx = noderxq.get(False)
+      rxq.put(nextrx)
+    except:
+      pass
+
+    #get next command
+    if cmdpipe.poll(0.5):
+      nextcmd = cmdpipe.recv()
+
+      if nextcmd == "GETID":
+        cmdpipe.send(node.nodeID)
+      elif nextcmd == "GETCONN":
+        cmdpipe.send(node.routeTable.getConnectedNodes())
+
+
+
+
+  #after node is killed, remove it from dict
   node.join()
-  nodeList.remove(node.nodeID)
+  del nodeDict[nodeId]
 
 def nodePrintCallback(nodeId, outputStr):
   globalNodeOutput.append("{0:04X}: {1}".format(nodeId, outputStr))
   
 def menu_addnode(scr):
   outputList = globalNodeOutput
-  p = multiprocessing.Process(target=startupNewNode, daemon=True)
+
+  #create queues/pipes for comm with new process
+  txq = multiprocessing.Queue()
+  rxq = multiprocessing.Queue()
+  pipeCon1, pipeCon2 = multiprocessing.Pipe()
+
+  p = multiprocessing.Process(target=startupNewNode, daemon=True, args=(txq, rxq, pipeCon2))
+
   p.start()
+
+  #wait for node to start and get node id
+  time.sleep(1)
+  pipeCon1.send("GETID")
+  nodeid = pipeCon1.recv()
+
+  nodeDict[nodeid] = (txq, rxq, pipeCon1)
+  print("Added node: {0}".format(nodeid))
 
 def menu_quit(scr):
   global uiRunning
   uiRunning = False
 
 def menu_print(scr):
-  
-  pass
+  subwin = scr.derwin(10, 10, 10, 10)
+  subwin.border()
+  subwin.addstr(1,1,"FFF")
+  subwin.refresh()
+  subwin.getch()
 
 def menu_sendmsg(scr):
-  pass
+  subwin = scr.derwin(5, 40, 10, 20)
+
+  #get source node
+  subwin.border()
+  subwin.addstr(1, 1, "Choose Sender: ")
+  subwin.refresh()
+  curses.echo()
+  srcnode = subwin.getstr()
+
+  #get dest node
+  subwin.erase()
+  subwin.border()
+  subwin.addstr(1, 1, "Choose Dest: ")
+  subwin.refresh()
+  curses.echo()
+  destnode = subwin.getstr()
+
+  #get message
+  subwin.erase()
+  subwin.border()
+  subwin.addstr(1, 1, "Enter Message: ")
+  subwin.refresh()
+  curses.echo()
+  messagecontents = str(subwin.getstr())
+
+  srcnodeint = int(srcnode, 16)
+  destnodeint = int(destnode, 16)
+
+  if srcnodeint in nodeDict:
+    nodeDict[srcnodeint][0].put(somnMesh.somnData(destnodeint, messagecontents))
+  else:
+    subwin.erase()
+    subwin.border()
+    subwin.addstr(1,1, "Invalid Source Node")
+    subwin.addstr(2,1, "(Press any key)")
+    subwin.getch()
+
+  subwin.getch()
 
 menuOptions = [
     ('A', 'Add Node', menu_addnode),
-    ('K', 'Kill Nodes', None),
-    ('Q', 'Quit', menu_quit),
-    ('P', 'Print Graph', menu_print)
-    ('S', 'Send Message', menu_sendmsg)]
+    #('K', 'Kill Nodes', None),
+    ('P', 'Print Graph', menu_print),
+    ('S', 'Send Message', menu_sendmsg),
+    ('Q', 'Quit', menu_quit)]
 
 def drawMenu(scr):
   x = 4
@@ -61,7 +157,7 @@ def drawMenu(scr):
 
 def drawNodeOutput(scr):
   maxlines = 20
-  x = 30
+  x = 50
   y = 5
   scr.addstr(y, x, "NODE OUTPUT:")
   y += 1
@@ -75,22 +171,26 @@ def drawNodeOutput(scr):
       y += 1
 
 def drawNodeList(scr):
-  x = 80
+  x = 30
   y = 5
+  maxy = y + 20
   scr.addstr(y, x, "NODE LIST:")
   y += 1
 
-  for line in nodeList:
-    scr.addstr(y, x, "{0:04X}".format(line))
+  for nodeid in nodeDict:
+    if y > maxy:
+      scr.addstr(y, x, "...")
+      break
+    scr.addstr(y, x, "{0:04X}".format(nodeid))
     y += 1
   
 def uiMain(mainscr):
-  mainscr.timeout(500)
   while uiRunning:
     screenwidth = mainscr.getmaxyx()[1]
     screenheight = mainscr.getmaxyx()[0]
 
 
+    mainscr.timeout(500)
     mainscr.clear()
     mainscr.border()
     mainscr.addstr(1,1, "SOMN Control Panel")
@@ -101,6 +201,7 @@ def uiMain(mainscr):
     drawNodeList(mainscr)
 
     mainscr.refresh()
+
     mainscr.addstr(3,1, "Active Processes: {0}".format(len(multiprocessing.active_children())))
 
     c = mainscr.getch()
